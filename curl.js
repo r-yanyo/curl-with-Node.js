9a915e6a9d25f5d26b0f48c092cc44f03552aa41
r-yanyo <honma.ryosuke@gmail.com>
2017-09-02 13:49:01 +0900

    implement parsing header

--- curl.js
+++ curl.js
@@ -1,6 +1,5 @@
 const request = require('request');
 const commander = require('commander');
-const parseHttpHeader = require('parse-http-header');
 
 commander
     .option('-H,--header <text>')
@@ -8,6 +7,15 @@ commander
     .option('-X,--request <method>','http method',/^(GET|POST)$/i,'GET')
     .option('-d,--data <data>','request body')
 commander.parse(process.argv);
+
+//parse header
+const headerSplit = commander.header.split(/: /);
+const headName = headerSplit[0].replace(/-./g,(s)=>s[1].toUpperCase(1)); //replace it to camelCase
+const headValue = headerSplit[1];
+
+//add header into commander
+commander[headName] = headValue;
+
 console.log(commander);
 
 const options = {
